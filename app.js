fc49682a256fe5998438d399f9ea4d6e20dc89ad
Tolga Tezel <tolgatezel11@gmail.com>
2012-04-30 16:44:36 -0400

    library loading speed increase

--- app.js
+++ app.js
@@ -1,111 +1,97 @@
 // Module dependencies.
 
 var express = require('express')
-    , fs = require('fs')
-    , findit = require('findit')
-    , path = require('path')
-    , util = require('util');
+  , fs = require('fs')
+  , findit = require('findit')
+  , path = require('path')
+  , util = require('util');
 
 var app = module.exports = express.createServer()
-    , io = require('socket.io').listen(app);
+  , io = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set("view options", {layout: false});
+  app.set('views', __dirname + '/views');
+  app.set("view options", {layout: false});
 
-    //html rendering
-    app.register('.html', {
-        compile: function(str, options){
-            return function(locals){
-                return str;
-            };
-        }
-    });
-  
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
+  //html rendering
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+
+  //app.use(express.bodyParser());
+  //app.use(express.methodOverride());
+  //app.use(app.router);
+  app.use(express.static(__dirname + '/public'))
+})
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+})
 
 app.configure('production', function(){
-    app.use(express.errorHandler()); 
-});
+  app.use(express.errorHandler())
+})
+
+app.songs = {} //for server song-lookup
+app.formats = ['.mp3', '.m4a'] //supported formats
+
+//cache music library for socket connections
+;(function () {
+  var finder = findit.find(__dirname + '/public/music')
+  // Populate music library
+  finder.on('file', function(fpath, stat) {
+    var ext = path.extname(fpath)
+    if(app.formats.indexOf(ext) !== -1) {
+      var songTitle = path.basename(fpath, ext)
+      app.songs[songTitle] = fpath;
+    }
+  })
+  //now listen for socket conn's
+  finder.on('end', function () {
+    io.sockets.on('connection', function(socket) {
+      Object.keys(app.songs).forEach(function (songName) {
+        socket.emit('song', { songName: songName })
+      })
+    })
+  })
+}).call(this)
 
 // Routes
 
 app.get('/', function(req, res){
-    res.render('index.html', {
-        locals: {
-          title: 'Streamy client'
-        }
-    });
-});
+  res.render('index.html', {
+    locals: {
+      title: 'Streamy client'
+    }
+  })
+})
 
 //Serve streaming audio - audio src points here
-
 app.get('/stream/:song', function(req, res) {
-    var songDetails = app.details[req.params.song]
-        , songPath = songDetails.path + '.' + songDetails.ext
-        , filePath = path.normalize(songPath)
-        , stat = fs.statSync(filePath)
-        , readStream = fs.createReadStream(filePath);
+  var songPath = app.songs[req.params.song]
 
-    res.writeHead(200, {
-        'Content-Type': 'audio/mpeg'
-        , 'Content-Length': stat.size
-    });
+  path.exists(songPath, function (exists) {
+    if(!exists) {
+      var msg = 'File ' + songPath + 'not found :\'{'
+      console.log('STREAMY:', msg)
+      res.writeHead(404)
+      res.end(msg)
+      return
+    }
 
-    util.pump(readStream, res); //throttle song to client
-});
+    fs.stat(songPath, function (err, stats) {
+      res.writeHead(200, { 'Content-Type': 'audio/mpeg', 'Content-Length': stats.size })
+      var readStream = fs.createReadStream(songPath)
+      util.pump(readStream, res) //throttle song to client
+    })
+  })
+})
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
-app.details = {}; //for server song-lookup
-app.formats = ['mp3', 'm4a']; //supported formats
-
-// Populate music library
-
-function getLibrary(sock) {
-    app.songs = []; //sent to client
-    var finder = findit.find(__dirname + '/public/music');
-
-    finder.on('file', function(path, stat) {
-
-        var i = path.length
-            , ext = '';
-
-        while(i-- && path[i] != '.') {
-            ext = path[i] + ext;  //get file extension
-            path = path.substring(0, path.length-1);
-        }
-
-        path = path.substring(0, path.length-1);  //remove the '.'
-
-        if(~app.formats.indexOf(ext)) { //make sure file format is supported
-            var pathArr = path.split('/')
-                , songTitle = pathArr[pathArr.length-1];
-                            
-            app.songs.push({'title': songTitle});
-            app.details[songTitle] = {'path': path, 'ext': ext};
-        }
-    });
-    finder.on('end', function() {
-        sock.emit('libLoaded', {lib: app.songs});
-    });
-};
-
-//Socket.io goodness
-
-io.sockets.on('connection', function(socket) {
-    socket.on('getMusic', function() {
-        getLibrary(socket);
-    });
-});
+app.listen(3000)
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,73 +1,76 @@
 $(function() {   
      
-    /**
-            Models
-    **/
-    window.Song = Backbone.Model.extend({});
+  /**
+          Models
+  **/
+  window.Song = Backbone.Model.extend({});
 
-    /**
-            Collections
-    **/
-    window.Library = Backbone.Collection.extend({
-        model: Song
-    });
+  /**
+          Collections
+  **/
+  window.Library = Backbone.Collection.extend({
+    model: Song
+  });
 
-    /**
-            Views
-    **/
-    window.SongView = Backbone.View.extend({
-        initialize: function() {
-            _.bindAll(this, 'render', 'play');   //conserve 'this' object
-        },
-        events: {
-            'dblclick': 'play'
-        },
-        tagName: 'li',
-        render: function() {
-            $(this.el).html('<span>'+this.model.toJSON().title+'</span>');
-            return this;
-        },
-        play: function() {
-             $('span#now-playing').html('Playing: ' + this.model.toJSON().title);
+  /**
+          Views
+  **/
+  window.SongView = Backbone.View.extend({
+    initialize: function() {
+      _.bindAll(this, 'render', 'play');   //conserve 'this' object
+    },
+    events: {
+      'dblclick': 'play'
+    },
+    tagName: 'li',
+    render: function() {
+      $(this.el).html('<span>'+this.model.toJSON().title+'</span>');
+      return this;
+    },
+    play: function() {
+       $('span#now-playing').html('Playing: ' + this.model.toJSON().title);
 
-            //stream the requested song
-            var player = $('audio#player')[0];
-            player.src='stream/'+this.model.toJSON().title;
-            player.play();
-        }
-    });
+      //stream the requested song
+      var player = $('audio#player')[0];
+      player.src='stream/'+this.model.toJSON().title;
+      player.play();
+    }
+  });
 
-    window.LibraryView = Backbone.View.extend({
-        initialize: function() {
-            _.bindAll(this, 'render');
-            this.collection.bind('add', this.appendItem);
-            this.render();
-        },
-        el: $(' div#library'),
-        render: function() {
-            $(this.el).append('<ul></ul>');
-            _.each(this.collection.models, function(item){   //append each model to the ul
-                this.appendItem(item);
-            }, this);
-            return this;
-        },
-        appendItem: function(item){
-            var item = new SongView({model: item});
-            $('ul', this.el).append(item.render().el);
-        }
-    });
+  window.LibraryView = Backbone.View.extend({
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.collection.bind('add', this.appendItem);
+      this.render();
+    },
+    el: $(' div.library'),
+    render: function() {
+      $(this.el).append('<ul></ul>');
+      //append each model to the ul
+      _.each(this.collection.models, function(item){
+        this.appendItem(item);
+      }, this);
+      return this;
+    },
+    appendItem: function(item){
+      var item = new SongView({model: item});
+      $('ul', this.el).append(item.render().el);
+    }
+  });
 
-    //refactor this schwag
-    document.getElementById('socket.io').addEventListener('load', sockListener);
-    
-    function sockListener() {
-        socket.emit('getMusic');
-        socket.on('libLoaded', function(d) {
+  document.getElementById('socket.io').addEventListener('load', sockListener);
 
-            window.App = {}; 
-            App.library = new Library(d.lib);
-            App.libraryView = new LibraryView({collection: App.library});
-            
-        });
-    };
-});
+  //grab music library
+  function sockListener() {
+    window.App = {}
+    App.library = new Library()
+    App.libraryView = new LibraryView({ collection: App.library })
+
+    //add songs to library Collection as they come in
+    socket.on('song', function (d) {
+      var song = new Song({ title: d.songName })
+      App.library.add(song)
+    })
+    socket.emit('getMusic')
+  }
+})
